39af69bf331d621dc168878caa029f8796d0932c
anxious4glory <53601508+anxious4glory@users.noreply.github.com>
2023-08-13 09:19:22 +0100

    -app overhaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "./src/**/*.{html,js}",
     "./public/index.html",
     "./public/styles/default.css",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite/"
   ],
   theme: {
     colors: {
